dc61e95ecd5bc1e406fd1accb839aa536a8a3e79
jeiizou <zhoujie01@souche.com>
2021-04-19 14:00:17 +0800

    feat: 新增一个异步请求的控制工具库

---
+++ packages/async-controller/__tests__/async-controller.test.js
@@ -0,0 +1,7 @@
+'use strict';
+
+const asyncController = require('../src/index');
+
+describe('@jeiizou/async controller', () => {
+    it('needs tests');
+});
---
+++ packages/async-controller/src/index.ts
@@ -0,0 +1,5 @@
+/**
+ * @jeiizou/async-controoler 异步控制库
+ */
+
+export const AsyncController = {};
