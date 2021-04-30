30decd2fed17c430e688641bc0926ed009894077
jeiizou <zhoujie01@souche.com>
2021-04-30 13:43:00 +0800

    unit: add url test

--- packages/http-request/__tests__/url.test.ts
+++ packages/http-request/__tests__/url.test.ts
@@ -1,5 +1,13 @@
-describe('url function test', () => {
-    test('decode test', () => {});
+import { buildURL } from '../src/util/url';
 
-    test('buildUrl test', () => {});
+describe('url function test', () => {
+    test('decode test', () => {
+        const url = 'http://www.abc.test';
+        const params = {
+            a: '1',
+            b: 2,
+        };
+        const urlString = buildURL(url, params);
+        expect(urlString).toBe('http://www.abc.test?a=%221%22&b=2');
+    });
 });
